553b6bc24db9511808ef7738c962f7f51504bf49
ZenBody <96446641+ZenBody@users.noreply.github.com>
2022-02-01 11:25:21 +0200

    Add files via upload

---
+++ Calculator App/Calculator App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator App
+//
+//  Created by Andy Ionascu on 29.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
